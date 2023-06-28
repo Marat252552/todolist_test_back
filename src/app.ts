0f6443cfd341ab4630bf77f9b385b13cc826344e
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-28 04:23:41 +0300

    1.1.1

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ app.use(cors({
 app.use(
     cookieSession({
         secret: 'gvdfgbvsreghbtrsfhb',
+        domain: 'front.marat252552.com',
         sameSite: 'none',
         secure: true,
         httpOnly: true,
